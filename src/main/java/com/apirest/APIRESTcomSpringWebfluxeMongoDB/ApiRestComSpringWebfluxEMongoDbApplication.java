44b549b2ed4c64417a070ce9d3a2ac858d875192
michelspirlandeli <michelspirlandeli10@gmail.com>
2020-01-19 10:57:26 -0300

    add project

---
+++ src/main/java/com/apirest/APIRESTcomSpringWebfluxeMongoDB/ApiRestComSpringWebfluxEMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.apirest.APIRESTcomSpringWebfluxeMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestComSpringWebfluxEMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestComSpringWebfluxEMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apirest/APIRESTcomSpringWebfluxeMongoDB/ApiRestComSpringWebfluxEMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.APIRESTcomSpringWebfluxeMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestComSpringWebfluxEMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
